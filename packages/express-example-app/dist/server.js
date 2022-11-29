b5cf474b76a9f7d34ff750e5be626837f310ea3b
“JoanRoucoux” <joan.roucoux@gmail.com>
2022-11-30 00:17:04 +0100

    [EVO] Add react example app

--- packages/express-example-app/dist/server.js
+++ packages/express-example-app/dist/server.js
@@ -6,8 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3001;
+app.use((_req, res, next) => {
+    // Allow any website to connect
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // Continue to next middleware
+    next();
+});
 app.get("/data", (req, res) => {
-    res.json({ foo: "bar" });
+    res.json({ value: "my awesome data" });
 });
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
--- packages/express-example-app/server.ts
+++ packages/express-example-app/server.ts
@@ -2,6 +2,14 @@ import express from 'express';
 const app = express();
 const port = 3001;
 
+app.use((_req, res, next) => {
+  // Allow any website to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Continue to next middleware
+  next();
+});
+
 app.get("/data", (req, res) => {
   res.json({ value: "my awesome data" });
 });
